6059480f6db4f7423d48ad18d3a79659f99e08ea
Rahul Shah <rshah98626@gmail.com>
2017-06-19 13:45:47 +0000

    Added Stripe response handler and erro handling js

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,67 @@
+/* global $, Stripe */
+
+//Document ready
+$(document).on('turbolinks:load', function(){
+  var theForm = $('#pro_form');
+  var submitBtn = $('#form-submit-btn');
+  //Set Stripe public key
+  Stripe.setPublishableKey( $('meta[name="stripe-key"]').attr('content') );
+  
+  //When user clicks form submit btn
+  submitBtn.click(function(event){
+    //prevent default submission behavior
+    event.preventDefault();
+    submitBtn.val("Processing").prop('disabled', true);
+    
+    //Collect the credit card fields
+    var ccNum = $('#card_number').val(),
+      cvcNum = $('#card_code').val(),
+      expMonth = $('#card_month').val(),
+      expYear = $('#card_year').val();
+      
+    //Validate cc fields with Stripe
+    var error = false;
+    
+    if(!Stripe.card.validateCardNumber(ccNum)){
+      error = true;
+      alert('The credit card number appears to be invalid.');
+    }
+    if(!Stripe.card.validateCVC(cvcNum)){
+      error = true;
+      alert('The CVC number appears to be invalid.');
+    }
+    if(!Stripe.card.validateExpiry(expMonth, expYear)){
+      error = true;
+      alert('The expiration month or year appears to be invalid.');
+    }
+    
+    if(error){
+      //if there are card errors, don't send to Stripe
+      submitBtn.prop('disabled', false).val("Sign Up");
+    } else{
+      //Send card info to Stripe
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
+    }
+    
+    return false;
+  });
+  
+  //Stripe returns a card token
+  function stripeResponseHandler(status, response){
+    //get token from response
+    var token = response.id;   
+    
+    //Inject it as a hidden field into form
+    theForm.append( $('<input type="hidden", name="user[stripe_card_token]">').val(token) );
+    
+    //Submit form to Rails app
+    theForm.get(0).submit();
+  }
+});
+
+
