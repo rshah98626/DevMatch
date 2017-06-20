83ac9a8a37f74cdac9fe3abc46830217e7612204
Rahul Shah <rshah98626@gmail.com>
2017-06-20 13:42:05 +0000

    Typo in user model file

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,16 +8,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
    # Otherwise signs user up like usual
    
    def create
-       super do |resource|
-           if params[:plan]
-               resource.plan_id = params[:plan]
-               if resource.plan_id == 2
-                  resource.save_with_subscription
-               else
-                  resource.save 
-               end
-           end
-       end
+    super do |resource|
+      if params[:plan]
+        resource.plan_id = params[:plan]
+        if resource.plan_id == 2
+          resource.save_with_subscription
+        else
+          resource.save
+        end
+      end
+    end
    end
    
    private
