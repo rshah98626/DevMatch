d75b60c42f36f7fe2db29db24059e625ba0f3af4
Rahul Shah <rshah98626@gmail.com>
2017-06-21 14:24:10 +0000

    Added the new profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,8 +2,25 @@ class ProfilesController < ApplicationController
     
     # GET to /users/:user_id/new
     def new
-       # Render blank profile form
-       @profile = Profile.new
+        # Render blank profile form
+        @profile = Profile.new
     end
     
+    #POST to user_profile_path
+    def create
+        # Ensure the we have the user filling out the forms
+        @user = User.find(params[:user_id])
+        @profile = @user.build_profile( profile_params )
+        if @profile.save
+            flash[:success] = "Profile updated!"
+            redirect_to root_path
+        else
+            render action: :new
+        end
+    end
+    
+    private
+        def profile_params
+           params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description) 
+        end
 end
