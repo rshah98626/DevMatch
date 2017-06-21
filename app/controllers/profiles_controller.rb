86f40e96ca1f74ee675c7f308f5e2de7922fb670
Rahul Shah <rshah98626@gmail.com>
2017-06-21 13:57:39 +0000

    Added new profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,8 @@ class ProfilesController < ApplicationController
     
     # GET to /users/:user_id/new
     def new
-       # Render blank profiles details form 
+       # Render blank profile form
+       @profile = Profile.new
     end
     
 end
