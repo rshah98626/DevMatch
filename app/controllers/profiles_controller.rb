2e19f517f1a6c61a4c2c504bf2455388d18c6303
Rahul Shah <rshah98626@gmail.com>
2017-06-25 17:54:56 +0000

    Added edit action for user profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -13,15 +13,30 @@ class ProfilesController < ApplicationController
         @profile = @user.build_profile( profile_params )
         if @profile.save
             flash[:success] = "Profile updated!"
-            redirect_to user_path(params[:user_id])
+            redirect_to user_path(id: params[:user_id])
         else
         end
     end
     
     # GET request to edit users' profiles
     def edit
-        @user = User.find(params[:user_id])
+        @user = User.find( params[:user_id] )
         @profile = @user.profile
+    end
+    
+    # PUT/PATCH request to update user
+    def update
+        # retrieve user from database
+        @user = User.find( params[:user_id] )
+        @profile = @user.profile
+        # Mass assign if attrs had changed
+        if @profile.update_attributes(profile_params)
+            flash[:success] = "Profile updated"
+            # Direct user to their new profile page
+            redirect_to user_path(id: params[:user_id])
+        else
+            render action: :edit
+        end
     end
     
     private
